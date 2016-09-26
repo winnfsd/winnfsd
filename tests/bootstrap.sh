628213bad630019db791c665c588c5d6a2735980
Marc Harding <info@marcharding.de>
2016-09-26 22:26:55 +0200

    Update to php 7 to maybe fix the composer segfault?

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+apt-get update
 sudo apt-get install -y git
-sudo apt-get install -y php5-cli php5-common php5-curl php5-intl php5-mcrypt php5-readline php5-json
+sudo apt-get install -y php7.0-cli php7.0-common php7.0-curl php7.0-intl php7.0-mcrypt php7.0-readline php7.0-json
